4a151438f5705936775c4f2917365514c6ba1ad4
rollrat <rollrat@inha.edu>
2019-11-06 09:55:21 +0900

    import: colored crate

---
+++ src/codegen/mod.rs
@@ -0,0 +1,2 @@
+//! This code is a part of py2rust project.
+//! Copyright (C) 2019. rollrat. Licensed under the MIT Licence.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
+//! This code is a part of py2rust project.
+//! Copyright (C) 2019. rollrat. Licensed under the MIT Licence.
 
+extern crate clap;
+extern crate colored;
+
+use clap::{Arg, ArgMatches, App, SubCommand};
+
+use colored::*;
+
+use std::fs;
+use std::path::Path;
+use std::process::exit;
 
 fn main() {
+    let app = App::new("py2rust");
+    let matched = app
+        .version("0.1")
+        .arg(Arg::with_name("input")
+            .short("v")
+            .long("input")
+            .index(1)
+            .required(true)
+            .takes_value(true)
+            .help("Input file path"))
+        .get_matches();
+
+    let filename = matched.value_of("input").unwrap();
+
+    if !Path::new(filename).exists() {
+        println!("{}: '{}' file not found!", "error".red().bold(), filename);
+        exit(1);
+    }
     
+    let raw = fs::read_to_string(filename).unwrap();
+
+    print!("{}", raw);
 }

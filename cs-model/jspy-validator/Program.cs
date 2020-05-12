b70dbc6a0dbb1d2e449669831fc07a9d47f1f5dc
rollrat <rollrat.cse@gmail.com>
2020-05-12 13:43:17 +0900

    cs-model: Add dynamic language validator

---
+++ cs-model/jspy-validator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jspy_validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
